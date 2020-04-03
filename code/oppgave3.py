207f0d3f39a7d54da9c39801f79b5519a9040db6
jobrajac <jonas4a@gmail.com>
2020-04-03 13:16:59 +0200

    Oppgave 3

--- code/oppgave3.py
+++ code/oppgave3.py
@@ -9,14 +9,18 @@ length = 2.0
 width = 0.3
 thickness = 0.03
 density = 480.0  # kg/m^3
-g = -9.8  # gravity, m/s^2
+g = 9.8  # gravity, m/s^2
 E = 1.3 * pow(10, 10)  # N/m^2
 I = (width * pow(thickness, 3)) / 12.0
 f = - density * width * thickness * g
 h = length/n
 
-matrixB = [((pow(h, 4)/(E * I))) * f] * n
 matrixA = generateA(n)
+matrixB = np.array([[(pow(h, 4) / (E * I)) * f]] * n)
 
+print(matrixA)
+print(matrixB)
 matrixY = solve(matrixA, matrixB)
 print(matrixY)
+# Check solution
+# print("Solution: " + str(np.allclose(np.dot(matrixA, matrixY), matrixB)))
