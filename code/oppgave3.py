3e8a0e653bdd91cdcc488c2ce50653e8272bc8aa
Lovster <sebastian.elvemo@gmail.com>
2020-04-16 12:50:17 +0200

    ( ͡° ͜ʖ ͡°)

--- code/oppgave3.py
+++ code/oppgave3.py
@@ -2,22 +2,30 @@ from oppgave2 import generateA
 import numpy as np
 from scipy.linalg import solve
 
+#Gitte bjelkeparametre.
 length = 2.0
 width = 0.3
 thickness = 0.03
 density = 480.0  # kg/m^3
+# Andre konstanter.
 g = 9.8  # gravity, m/s^2
-E = 1.3 * pow(10, 10)  # N/m^2
-I = (width * pow(thickness, 3)) / 12.0
+E = 1.3 * pow(10, 10)  # Materialkonstanten N/m^2
+I = (width * pow(thickness, 3)) / 12.0 #Arealmomentet.
 f = - density * width * thickness * g
 
-
+#Løsning på Ay=b med størrelse (n)
 def solve_3(n):
+	#Deler bjelkens lengde så alle segmentene er like.
     h = length / n
+	#Genererer båndmatrise (A matrisen)
     matrixA = generateA(n)
+	#Genererer b-matrisen.
     matrixB = np.array([[(pow(h, 4) / (E * I)) * f]] * n)
+	#Løser for Y basert på A og B.
     matrixY = solve(matrixA, matrixB)
+	#Finner kondisjonstallet.
     kondisjonstall = np.linalg.cond(matrixA)
+	#Returnerer løsningen og kondisjonstallet.
     return matrixY, kondisjonstall
 
 
